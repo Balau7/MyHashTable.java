db14a4f3a2d1a7dcb10f32f3ce2d66ada91cabac
lanfynn <balausakttymbek@gmail.com>
2025-04-30 02:49:18 +0500

    MyHashTable.java

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,42 +1,110 @@
 import java.util.LinkedList;
 
 public class MyHashTable<K, V> {
-    private static class HashNode<K, V> {
-        private K key;
-        private V value;
-        private HashNode<K, V> next;
+    private class HashNode<K, V> {
+        K key;
+        V value;
+        HashNode<K, V> next;
 
         public HashNode(K key, V value) {
             this.key = key;
             this.value = value;
         }
+
+        @Override
+        public String toString() {
+            return "{" + key + " : " + value + "}";
+        }
     }
 
-    private LinkedList<HashNode<K, V>>[] buckets;
-    private int capacity;
+    private int M = 11; // Default number of buckets
+    private HashNode<K, V>[] chainArray;
     private int size;
 
     public MyHashTable() {
-        capacity = 10;
-        buckets = new LinkedList[capacity];
-        for (int i = 0; i < capacity; i++) {
-            buckets[i] = new LinkedList<>();
-        }
+        chainArray = new HashNode[M];
         size = 0;
     }
 
+    public MyHashTable(int capacity) {
+        this.M = capacity;
+        chainArray = new HashNode[M];
+        size = 0;
+    }
+
+    private int hash(K key) {
+        return Math.abs(key.hashCode()) % M;
+    }
+
     public void put(K key, V value) {
-        int index = Math.abs(key.hashCode()) % capacity;
-        buckets[index].add(new HashNode<>(key, value));
+        int bucketIndex = hash(key);
+        HashNode<K, V> head = chainArray[bucketIndex];
+
+        while (head != null) {
+            if (head.key.equals(key)) {
+                head.value = value;
+                return;
+            }
+            head = head.next;
+        }
+
+        HashNode<K, V> newNode = new HashNode<>(key, value);
+        newNode.next = chainArray[bucketIndex];
+        chainArray[bucketIndex] = newNode;
         size++;
     }
 
-    public int getCapacity() {
-        return capacity;
+    public V get(K key) {
+        int index = hash(key);
+        HashNode<K, V> head = chainArray[index];
+        while (head != null) {
+            if (head.key.equals(key)) return head.value;
+            head = head.next;
+        }
+        return null;
     }
 
-    public int getBucketSize(int index) {
-        return buckets[index].size();
+    public boolean contains(V value) {
+        for (int i = 0; i < M; i++) {
+            HashNode<K, V> head = chainArray[i];
+            while (head != null) {
+                if (head.value.equals(value)) return true;
+                head = head.next;
+            }
+        }
+        return false;
+    }
+
+    public K getKey(V value) {
+        for (int i = 0; i < M; i++) {
+            HashNode<K, V> head = chainArray[i];
+            while (head != null) {
+                if (head.value.equals(value)) return head.key;
+                head = head.next;
+            }
+        }
+        return null;
+    }
+
+    public V remove(K key) {
+        int index = hash(key);
+        HashNode<K, V> head = chainArray[index];
+        HashNode<K, V> prev = null;
+
+        while (head != null) {
+            if (head.key.equals(key)) {
+                if (prev != null) {
+                    prev.next = head.next;
+                } else {
+                    chainArray[index] = head.next;
+                }
+                size--;
+                return head.value;
+            }
+            prev = head;
+            head = head.next;
+        }
+        return null;
     }
 
     public int size() {
